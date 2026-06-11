878864940428bba72e81dd411b26c49cf2a58805
Samada98 <mark.lavachielli@me.com>
2026-06-11 19:40:56 +0200

    Fix Program cs for Render

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using YourApp.Hubs;   // <-- necessario per GameHub
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Fai in modo che ascolti su tutte le interfacce (0.0.0.0)
-builder.WebHost.UseUrls("http://192.168.1.105:5000");
-
-// ... resto del codice
 builder.Services.AddSignalR();
 builder.Services.AddControllersWithViews();
 
@@ -15,6 +10,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapHub<GameHub>("/gamehub"); // endpoint SignalR
+app.MapHub<GameHub>("/gamehub");
 
 app.Run();
